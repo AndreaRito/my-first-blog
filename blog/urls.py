635ecce26113f718fda0815ab9e192c56891ebf3
AndreaRito <andrearitondale94@gmail.com>
2020-11-20 13:13:56 +0100

    Ho cambiato l'HTML per questo sito.

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+# importato la funzione url di Django e tutte
+#  le nostre views dalla applicazione blog 
+
+
+#modello di URL
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Creiamo una vista come suggerisce il commento
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    
+#abbiamo creato una funzione (def) chiamata post_list 
+# che prende request e restituisce return una funzione
+#  render che renderizza (mette insieme) il nostro
+#  template blog/post_list.html.
+
