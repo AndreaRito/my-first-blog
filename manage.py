2b7bd2d74b9d5246acac20860f96a7656b72fbd7
AndreaRito <andrearitondale94@gmail.com>
2020-11-25 17:18:02 +0100

    Ho cambiato l'HTML per questo sito.

--- manage.py
+++ manage.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
-
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
